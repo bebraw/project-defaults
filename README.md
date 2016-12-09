69e29799a9fc9a6eac676c748708bb329fdb35fa
Juho Vepsalainen <bebraw@gmail.com>
2016-12-09 12:18:23 +0200

    Add package.json logic
    Now it merges and copies those so you get good defaults.

--- README.md
+++ README.md
@@ -5,9 +5,7 @@ My project defaults to share across projects
 **Usage:**
 
 1. `npm install @bebraw/project-defaults -D`
-2. Add `"postinstall": "project-defaults -t react"` to npm `"scripts"`. That will copy files around as you work.
-
-> This doesn't manage dependencies like Babel or ESLint yet. Still need to find a good approach for that.
+2. Add `"postinstall": "project-defaults -t react"` to npm `"scripts"`. That will copy files around as you work and patch `package.json` with good defaults you can override.
 
 ## License
 
--- src/index.js
+++ src/index.js
@@ -1,29 +1,44 @@
+const fs = require('fs');
 const path = require('path');
 const async = require('async');
 const cpr = require('cpr');
+const merge = require('webpack-merge');
 
-// Overlays target files on top of common template while copying
+// Overlays target files on top of common template while copying.
+// package.json is treated as a special case. We'll merge it instead.
 function copyTarget(from, to, target, cb) {
   // TODO: validate that target directory exists
   if (!target) {
     return cb(new Error('Missing target'));
   }
 
+  const commonPath = path.resolve(from, 'common');
+  const targetPath = path.resolve(from, target);
+
   async.series([
     copyTemplates.bind(
       null,
-      path.resolve(from, 'common'),
+      commonPath,
       to
     ),
     copyTemplates.bind(
       null,
-      path.resolve(from, target),
+      targetPath,
       to
+    ),
+    mergePackageJson.bind(
+      null,
+      to,
+      [
+        require(path.resolve(commonPath, 'package.json')),
+        require(path.resolve(targetPath, 'package.json')),
+        require(path.resolve(process.cwd(), 'package.json'))
+      ]
     )
   ], cb);
 }
 
-// Copies templates from a directory to another without overwriting
+// Copies templates from a directory to another without overwriting.
 function copyTemplates(from, to, cb) {
   cpr(from, to, {
       deleteFirst: false,
@@ -31,6 +46,16 @@ function copyTemplates(from, to, cb) {
       confirm: true
   }, cb);
 };
+
+function mergePackageJson(target, packages, cb) {
+  console.log(path.join(target, 'package.json'));
+
+  fs.writeFile(
+    path.join(target, 'package.json'),
+    JSON.stringify(merge.apply(null, packages), null, 2),
+    cb
+  );
+}
 
 exports.copyTarget = copyTarget;
 exports.copyTemplates = copyTemplates;
